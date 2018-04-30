25e094c5c577bad238b4a9e8527b499ca880b450
Jeremiah <jeremiahteee@gmail.com>
2018-04-30 15:06:04 -0400

    infinite creation of new obstacles using switch case

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -12,6 +12,26 @@ public class Controller : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision){
+		counter = Random.Range (1, 5);
+		if (collision.gameObject.tag == "Obstacle") {
+			switch (counter) {
+			case 1:
+				//create a clone of obs1 and add it to location of new obstacle
+				Instantiate (obs1, new Vector2(PositionNewObstacle.transform.position.x, obs1.transform.position.y), Quaternion.identity);
+				break;
+			case 2:
+				Instantiate (obs2, new Vector2(PositionNewObstacle.transform.position.x, obs2.transform.position.y), Quaternion.identity);
+				break;
+			case 3:
+				Instantiate (obs3, new Vector2(PositionNewObstacle.transform.position.x, obs3.transform.position.y), Quaternion.identity);
+				break;
+			case 4:
+				Instantiate (obs4, new Vector2(PositionNewObstacle.transform.position.x, obs4.transform.position.y), Quaternion.identity);
+				break;
+			}
+		}
 	}
 }
